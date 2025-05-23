10ecb1754320cb75eb73e461c07a84ebed2e0d72
rbmns <r.a.biemans@gmail.com>
2025-05-23 13:32:47 +0200

    Update footer.tsx

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -11,10 +11,13 @@ export const Footer: React.FC = () => {
         <div className="flex flex-col md:flex-row justify-between items-center text-sm text-gray-600">
           <div className="mb-2 md:mb-0">
             <p>&copy; {currentYear} the lineup. All rights reserved. </p>
-</div><div>      
+</div>
+         <div className="flex justify-center">
             <a 
-              href="https://humblestudio.ai/?utm_source=thelineup&utm_medium=footer&utm_campaign=label" target="_blank" rel="noopener noreferrer"
-              className="flex justify-center"
+              href="https://humblestudio.ai/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="hover:opacity-80 transition-opacity"
             >
               <img 
                 src="https://res.cloudinary.com/dita7stkt/image/upload/v1747993915/MadeByHS_kry9in.png" 
@@ -22,7 +25,11 @@ export const Footer: React.FC = () => {
                 className="h-14"
               />
             </a>
-</div>          </div>
+          </div>
+        
+        
+        
+        </div>
           <div className="flex space-x-5">
             <Link to="/privacy" className="hover:text-gray-900 transition">
               Privacy Policy
