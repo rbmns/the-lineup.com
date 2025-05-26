ef151026e8459ad89619778d8ed10551ca0fc2ae
rbmns <r.a.biemans@gmail.com>
2025-05-26 23:39:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,14 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; img-src 'self' https://raw.githubusercontent.com data:;",
+    },
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
